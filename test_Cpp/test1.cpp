f029541bb8aad7749e840de765396afbc902185d
2281469043 <changk2@rpi.edu>
2023-06-23 14:39:41 -0400

    Update test1.cpp
    update

--- test_Cpp/test1.cpp
+++ test_Cpp/test1.cpp
@@ -43,3 +43,31 @@ int main()
     WSACleanup();
     return 0;
 }
+
+void mParseUrl(char *mUrl, string &serverName, string &filepath, string &filename)
+{
+    string::size_type n;
+    string url = mUrl;
+
+    if (url.substr(0,7) == "http://")
+        url.erase(0,7);
+
+    if (url.substr(0,8) == "https://")
+        url.erase(0,8);
+
+    n = url.find('/');
+    if (n != string::npos)
+    {
+        serverName = url.substr(0,n);
+        filepath = url.substr(n);
+        n = filepath.rfind('/');
+        filename = filepath.substr(n+1);
+    }
+
+    else
+    {
+        serverName = url;
+        filepath = "/";
+        filename = "";
+    }
+}
