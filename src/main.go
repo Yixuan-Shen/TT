b7244941c2f4d04f9667ef92123b5cb6ed30a9c9
RyanLil-Xvx <91490019+RyanLil-Xvx@users.noreply.github.com>
2023-07-03 03:35:43 -0400

    New PATCH function

--- src/main.go
+++ src/main.go
@@ -88,11 +88,13 @@ func initializeDL() (deviceList *list.List) {
 	return deviceList
 }
 
+// Update the data in the device list, using PushBack
 func updateDeviceList(nearbyDeviceList *list.List, name string, distance float64) *list.List {
 	nearbyDeviceList.PushBack(Device{uuid.New(), name, distance})
 	return nearbyDeviceList
 }
 
+// Clear all the information in device list
 func clearDeviceList(deviceList *list.List) {
 	deviceList = list.New()
 }
@@ -136,12 +138,13 @@ func homepage(w http.ResponseWriter, r *http.Request) {
 	fmt.Println("Endpoint Hit: homepage")
 }
 
+// The function "GET"
 func getDeviceWithID(w http.ResponseWriter, r *http.Request) {
 	vars := mux.Vars(r)
 	uuidStr := vars["uuid"]
 	UUID, _ := uuid.Parse(uuidStr)
 
-	fmt.Println(`UUID: ` + uuidStr)
+	fmt.Println("UUID: " + uuidStr)
 	fmt.Println("Endpoint Hit: getDeviceWithID")
 
 	for _, device := range devices {
@@ -152,29 +155,31 @@ func getDeviceWithID(w http.ResponseWriter, r *http.Request) {
 		}
 	}
 
-	fmt.Println("No device found")
-	fmt.Fprintf(w, "No device found")
+	fmt.Println(" No device found")
+	fmt.Fprintf(w, " No device found")
 }
 
+// The function "DELETE"
 func deleteDeviceWithID(w http.ResponseWriter, r *http.Request) {
 	vars := mux.Vars(r)
 	uuidStr := vars["uuid"]
 	UUID, _ := uuid.Parse(uuidStr)
 
-	fmt.Fprintf(w, `UUID: `+uuidStr)
+	fmt.Fprintf(w, "UUID: "+uuidStr)
 	fmt.Println("Endpoint Hit: deleteDeviceWithID")
 
 	for index, device := range devices {
 		if device.ID == UUID {
 			devices = append(devices[:index], devices[index+1:]...)
-			fmt.Fprintf(w, "Device deleted")
+			fmt.Fprintf(w, " Device deleted")
 			return
 		}
 	}
 
-	fmt.Fprintf(w, "No device found")
+	fmt.Fprintf(w, " No device found")
 }
 
+// The function "POST"
 func addDeviceWithID(w http.ResponseWriter, r *http.Request) {
 	fmt.Println("Endpoint Hit: addDeviceWithID")
 	requestBody, _ := ioutil.ReadAll(r.Body)
@@ -192,6 +197,33 @@ func addDeviceWithID(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Device added")
 }
 
+// The function "PATCH"
+func modifyDeviceWithID(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("Endpoint Hit: modifyDeviceWithID")
+	requestBody, _ := ioutil.ReadAll(r.Body)
+	var newDevice Device
+	json.Unmarshal(requestBody, &newDevice)
+	for _, device := range devices {
+		if device.ID == newDevice.ID {
+			if device.Name == "" {
+				fmt.Fprintf(w, " New name cannot be empty, plase re-enter a new one\n")
+			} else if device.Name == newDevice.Name {
+				fmt.Fprintf(w, " New name cannot be the same as before\n")
+			} else {
+				fmt.Fprintf(w, " Device name update successfully\n")
+			}
+			if device.Distance_m == newDevice.Distance_m {
+				fmt.Fprintf(w, " New distance cannot be the same as before\n")
+			} else {
+				fmt.Fprintf(w, " Device distance update successfully\n")
+			}
+			return
+		}
+	}
+	devices = append(devices, newDevice)
+	fmt.Fprintf(w, " Device ID: %s, info changed", newDevice.ID)
+}
+
 // Handle the function link to the website
 func handleRequests() {
 	myRouter := mux.NewRouter().StrictSlash(true)
@@ -202,6 +234,7 @@ func handleRequests() {
 	myRouter.HandleFunc("/device", addDeviceWithID).Methods("POST")
 	myRouter.HandleFunc("/device/{uuid}", getDeviceWithID).Methods("GET")
 	myRouter.HandleFunc("/device/{uuid}", deleteDeviceWithID).Methods("DELETE")
+	myRouter.HandleFunc("/device/{uuid}/name={newName}/distance={newDistance}", modifyDeviceWithID).Methods("PATCH")
 
 	myRouter.HandleFunc("/currentOnewayDistance", onewayDistancePage)
 	myRouter.HandleFunc("/connection", connection)
@@ -209,6 +242,7 @@ func handleRequests() {
 	log.Fatal(http.ListenAndServe(":10000", myRouter))
 }
 
+// Global variable for the devices array
 var devices = []Device{
 	Device{uuid.New(), "Device1", 50},
 	Device{uuid.New(), "Device2", 30},
